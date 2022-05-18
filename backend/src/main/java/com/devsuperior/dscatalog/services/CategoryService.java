2d6a2dea18add2a6bdcba56859e19dd8326e8c4c
Vanessa Barone <vanbarone.milani@gmail.com>
2022-05-18 17:21:21 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repo;
+	
+	public List<Category> findAll(){
+		return repo.findAll();
+	}
+
+}
