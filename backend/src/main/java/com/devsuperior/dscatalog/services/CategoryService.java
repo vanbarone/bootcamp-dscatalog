113d9cdaa80efc3085550233857b12aa8e3029eb
Vanessa Barone <vanbarone.milani@gmail.com>
2022-05-19 12:00:20 -0300

    CategoryDTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -17,8 +20,26 @@ public class CategoryService {
 	private CategoryRepository repo;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repo.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repo.findAll();
+		
+		List<CategoryDTO> listDTO = new ArrayList<>();
+		
+		/* Pode ser feito dessa forma ou então com expressão lambda como foi feito a seguir
+		for (Category cat: list) {
+			listDTO.add(new CategoryDTO(cat));
+		}
+		*/
+		
+		listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		/*Expressão lambda explicação:
+		 * stream é para pegar todos os elementos da lista
+		 * map é para aplicar uma função a cada elemento da lista, no caso aqui está transformando cada elemento do tipo Category em CategoryDTO
+		 * collect é para que essa stream volte a ser uma lista 
+		 */
+		
+		return listDTO;
 	}
 
 }
